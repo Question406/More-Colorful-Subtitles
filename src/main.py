c97f8e8c1102cc126d01bec5a96670a91867ac60
jiyi <sjcs_jijiabao@sjtu.edu.cn>
2020-11-12 18:54:05 +0800

    use pycharm now and tested Pillow APIs

---
+++ src/main.py
@@ -0,0 +1,70 @@
+import os
+
+from PIL import ImageDraw
+from cv2 import cv2 as cv
+
+from utils import (drawImageSingleText, CV2ToPIL, PILToCV2, getTextInfoPIL, funcTime)
+
+
+# imagePath = "./videoSrc/test.png"
+# outputPath = "./temp.png"
+# outputF = open(outputPath, "wb")
+#
+# fontpath = "fonts/Dyuthi-Regular.ttf"
+# font = ImageFont.truetype(fontpath, 32)
+# color = (0, 255, 0)
+# # with Image.open(imagePath).convert("RGBA") as im:
+# with Image.open(imagePath) as im:
+#     draw = ImageDraw.Draw(im)
+#     drawImageSingleText(draw, 'Hello World!', font='Consolas', anchor=(0, 0))
+#     im.save(outputF, "PNG")
+# outputF.close()
+
+def _main(*args):
+    srcName = args[0]
+    videoSrc = "./videoSrc/%s.mp4" % srcName
+    outputDir = "./videoOutput/%s" % srcName
+    if not os.path.exists(outputDir):
+        os.mkdir(outputDir)
+
+    result_video = "%s/%s-Subtitle.mp4" % (outputDir, srcName)
+    cap = cv.VideoCapture(videoSrc)
+    print("read done")
+    # video FPS
+    fps_video = cap.get(cv.CAP_PROP_FPS)
+    # video format
+    fourcc = cv.VideoWriter_fourcc(*"mp4v")
+    # frame width and height
+    frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    videoWriter = cv.VideoWriter(result_video, fourcc, fps_video, (frame_width, frame_height))
+    frame_id = 0
+    print("transfering %s" % srcName)
+    print(frame_width, " ", frame_height)
+    output = []
+    k = 5
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            frame_id += 1
+            if frame_id % 100 == 0:
+                print("hello", frame_id // 100)
+            # text coordinate
+            text = 'frame_%s' % frame_id
+            imgH, imgW = frame_height, frame_width
+            fontScale, fontThickness = 1.5, 2
+            im = CV2ToPIL(frame)
+            draw = ImageDraw.Draw(im)
+            textWidth, textHeight = getTextInfoPIL(draw, text)
+            drawImageSingleText(draw, text, font='Consolas', anchor=(imgW // 2 - textWidth // 2, imgH - k * textHeight))
+            frame = PILToCV2(im)
+            videoWriter.write(frame)
+
+        else:
+            videoWriter.release()
+            break
+
+    return 0
+
+
+funcTime(_main, 'demo_Trim')
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,81 @@
-import cmath
+import time
 
-def get
+import numpy as np
+from PIL import Image, ImageFont
+from cv2 import cv2 as cv
+
+
+def getFont(font='Consolas', fontSize=32):
+    """
+    :param font: font name
+    :param fontSize: font size
+    :return: an ImgeFont object
+    """
+    fontPath = "fonts/%s.ttf" % font
+    return ImageFont.truetype(fontPath, fontSize)
+
+
+def drawImageSingleText(image, text, font='Consolas', anchor=(0, 0), color=(0, 255, 0), fontSize=32):
+    """
+    :param image: a ImageDraw object to draw text
+    :param anchor: left upper point of the text as PIL wanted
+    :param text: text to draw
+    :param font: font of text
+    :param color: color of text
+    :return: a Image object from PIL
+    """
+    # get font
+    font = getFont(font, fontSize)
+    # draw text
+    image.text(anchor, text, font=font, fill=color)
+    # bounding box of entire text
+    bbox = image.textbbox(anchor, text, font=font)
+
+    # draw the rectangle bounding text
+    image.rectangle(bbox, fill=None, outline=color)
+
+    # left upper bound of bbox
+    lastl, u = anchor[0], anchor[1]
+    image.rectangle((lastl, u, lastl, u), fill=None, outline=(255, 0, 0))
+
+    # draw singleText box
+    for ch in text:
+        box = image.textbbox((lastl, u), ch, font=font)
+        image.rectangle(box, fill=None, outline=(255, 0, 0))
+        lastl = box[2]
+
+    return image
+
+
+def getTextInfoPIL(image, text, font='Consolas', fontSize=32):
+    """
+    :param image: PIl image object
+    :param text: text we want to draw
+    :param font: font name
+    :param fontSize: font size
+    :return:
+    """
+    font = getFont(font, fontSize)
+    return image.textsize(text, font)
+
+
+def CV2ToPIL(image):
+    """
+    :param image: numpy array (cv2 accept)
+    :return: PIL image object
+    """
+    return Image.fromarray(cv.cvtColor(image, cv.COLOR_BGR2RGB))
+
+
+def PILToCV2(image):
+    """
+    :param image: PIL image object
+    :return: numpy array (cv2 accpet)
+    """
+    return cv.cvtColor(np.asarray(image), cv.COLOR_RGB2BGR)
+
+
+def funcTime(func, *args, **kwargs):
+    start = time.clock()
+    func(*args)
+    print("elapsed: ", time.clock() - start)
