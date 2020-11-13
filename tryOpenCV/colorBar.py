9ee76cb4aa4896bdab50e495b741e97320bde17f
zytzrh <zjhz-zyt@sjtu.edu.cn>
2020-11-13 14:32:48 +0800

    add coloarBar generatation

--- tryOpenCV/colorBar.py
+++ tryOpenCV/colorBar.py
@@ -10,12 +10,37 @@ cmaps = OrderedDict()
 cmaps['Perceptually Uniform Sequential'] = [
             'viridis', 'plasma', 'inferno', 'magma', 'cividis']
 
-nrows = max(len(cmap_list) for cmap_category, cmap_list in cmaps.items())
-gradient = np.linspace(0, 1, 512)
-gradient = np.vstack((gradient, gradient))
+cmaps['Sequential'] = [
+            'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
+            'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+            'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']
+
+cmaps['Sequential (2)'] = [
+            'binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink',
+            'spring', 'summer', 'autumn', 'winter', 'cool', 'Wistia',
+            'hot', 'afmhot', 'gist_heat', 'copper']
+
+cmaps['Diverging'] = [
+            'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
+            'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']
+
+cmaps['Cyclic'] = ['twilight', 'twilight_shifted', 'hsv']
+
+cmaps['Qualitative'] = ['Pastel1', 'Pastel2', 'Paired', 'Accent',
+                        'Dark2', 'Set1', 'Set2', 'Set3',
+                        'tab10', 'tab20', 'tab20b', 'tab20c']
+
+cmaps['Miscellaneous'] = [
+            'flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern',
+            'gnuplot', 'gnuplot2', 'CMRmap', 'cubehelix', 'brg',
+            'gist_rainbow', 'rainbow', 'jet', 'turbo', 'nipy_spectral',
+            'gist_ncar']
+
 
 
 def plot_color_gradients(cmap_category, cmap_list, nrows):
+    gradient = np.linspace(0, 1, 512)
+    gradient = np.vstack((gradient, gradient))  # convert to 2-dimension
     fig, axes = plt.subplots(nrows=nrows)
     fig.subplots_adjust(top=0.95, bottom=0.01, left=0.2, right=0.99)
     axes[0].set_title(cmap_category + ' colormaps', fontsize=14)
@@ -32,7 +57,25 @@ def plot_color_gradients(cmap_category, cmap_list, nrows):
         ax.set_axis_off()
 
 
-for cmap_category, cmap_list in cmaps.items():
-    plot_color_gradients(cmap_category, cmap_list, nrows)
+# def showColorBar():
+#     nrows = max(len(cmap_list) for cmap_category, cmap_list in cmaps.items())
+#     for cmap_category, cmap_list in cmaps.items():
+#         plot_color_gradients(cmap_category, cmap_list, nrows)
+#
+#     plt.show()
 
-plt.show()
+def showColorBar(bar_kind, num):
+    cmap = plt.get_cmap(bar_kind)
+    gradient = np.linspace(0, 1, num)
+    gradient = np.vstack((gradient, gradient))  # convert to 2-dimension
+    fig, ax = plt.subplots(nrows=1)
+    fig.subplots_adjust(top=0.95, bottom=0.01, left=0.2, right=0.99)
+    ax.set_title(bar_kind + ' colormaps', fontsize=14)
+    ax.imshow(gradient, aspect='auto', cmap=cmap)
+    ax.set_axis_off()
+    plt.show()
+
+def getColorBar(bar_kind, num):
+    cmap = plt.get_cmap(bar_kind)
+    gradient = np.linspace(0, 1, num)
+    return cmap(gradient) * 255
